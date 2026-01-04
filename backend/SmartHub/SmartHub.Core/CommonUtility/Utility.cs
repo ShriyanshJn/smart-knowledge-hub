3bd0691abb0bf6b5e90b5cc29a4d05a30dd4a334
Shriyansh <shriyanshjain4444@gmail.com>
2026-01-04 23:30:18 +0530

    Add common password validation

---
+++ backend/SmartHub/SmartHub.Core/CommonUtility/Utility.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartHub.Core.CommonUtility
+{
+    public static class Utility
+    {
+        public static bool IsPasswordValid(string password)
+        {
+            if (password.Length < 8) return false;
+            if (!password.Any(char.IsLetter)) return false;
+            if (!password.Any(char.IsDigit)) return false;
+            return true;
+        }
+    }
+}
