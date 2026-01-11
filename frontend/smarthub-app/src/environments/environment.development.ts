dea36d51d35b7b81189d6cf5987540576f532f73
Shriyansh <shriyanshjain4444@gmail.com>
2026-01-11 18:48:24 +0530

    add env

---
+++ frontend/smarthub-app/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  authApiBaseUrl: 'https://localhost:44305/api/auth',
+  apiBaseUrl: 'https://localhost:44374/api'
+};
---
+++ frontend/smarthub-app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  authApiBaseUrl: 'https://www..com/api/auth',
+  apiBaseUrl: 'https://www..com/api'
+};
