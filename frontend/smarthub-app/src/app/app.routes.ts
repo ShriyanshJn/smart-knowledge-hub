8fe2833d0fa61ee10063eb73e9e980557c51fe82
Shriyansh <shriyanshjain4444@gmail.com>
2026-02-06 00:13:04 +0530

    add, update routes

--- frontend/smarthub-app/src/app/app.routes.ts
+++ frontend/smarthub-app/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [{ path: 'dashboard', component: DashboardComponent }],
+  },
+  { path: '**', redirectTo: 'dashboard' },
 ];
